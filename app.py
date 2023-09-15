2aa7c2f42441b2486ed4fd4b70f55ebdd1a8e992
Mehtab Singh <mehtabsinghxx@gmail.com>
2023-09-15 15:34:06 +0100

    User can now input stock of choice

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import yfinance as yf
 import matplotlib
-matplotlib.use('Agg')  # Use the 'Agg' backend (non-interactive) for Matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
 
 app = Flask(__name__)
 
-def generate_and_save_stock_graph():
-    # Define the stock symbol (Apple Inc. is AAPL)
-    stock_symbol = "GME"
-
-    # Fetch stock data
+def generate_and_save_stock_graph(stock_symbol):
     apple_stock = yf.Ticker(stock_symbol)
     stock_data = apple_stock.history(period="1y")
 
-    # Extract the Date and Closing Price data
     dates = stock_data.index
     closing_prices = stock_data["Close"]
 
-    # Create the stock graph
     plt.figure(figsize=(10, 6))
     plt.plot(dates, closing_prices, label=f'{stock_symbol} Closing Price', color='blue')
     plt.xlabel('Date')
@@ -29,22 +23,25 @@ def generate_and_save_stock_graph():
     plt.legend()
     plt.grid(True)
 
-    # Save the plot to a BytesIO object
     img = BytesIO()
-    plt.savefig(img, format='png')
+    plt.savefig('static/graph.png', format='png')
     img.seek(0)
     plt.close()
 
-    # Encode the image to base64 for HTML embedding
     img_base64 = base64.b64encode(img.read()).decode()
 
     return img_base64
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # Generate the stock graph and get the image data
-    img_data = generate_and_save_stock_graph()
+    stock_symbol = "AAPL"  # Default stock symbol
+    img_data = None
+
+    if request.method == 'POST':
+        stock_symbol = request.form['stock_symbol']
     
+    img_data = generate_and_save_stock_graph(stock_symbol)
+
     return render_template('index.html', img_data=img_data)
 
 if __name__ == '__main__':
