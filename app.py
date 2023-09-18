3fb4c9859a201182044b3201270f3cf37f9cc1d5
Mehtab Singh <mehtabsinghxx@gmail.com>
2023-09-18 13:20:32 +0100

    Added Random Forest Regressor model

--- app.py
+++ app.py
@@ -8,11 +8,19 @@ import base64
 import datetime
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor  
 import numpy as np
+
 
 app = Flask(__name__)
 
-def generate_and_save_stock_graph(stock_symbol):
+# Create a dictionary to map model names to their corresponding model objects
+models = {
+    "Linear Regression": LinearRegression(),
+    "Random Forest": RandomForestRegressor(n_estimators=100, random_state=42),  # Example model
+}
+
+def generate_and_save_stock_graph(stock_symbol, selected_ml_model):
     end_date = datetime.date.today()
     start_date = end_date - datetime.timedelta(days=365)
 
@@ -28,25 +36,28 @@ def generate_and_save_stock_graph(stock_symbol):
     poly = PolynomialFeatures(degree=3)
     X_poly = poly.fit_transform(days)
 
-    # Create and train a Polynomial Regression model for historical data
-    model = LinearRegression()
+    # Create and train the selected machine learning model for historical data
+    model = selected_ml_model
     model.fit(X_poly, closing_prices)
 
     # Generate dates for historical data and prediction (e.g., extending for 15 days ahead)
     all_dates = stock_data.index
-    future_dates = [all_dates[-1] + datetime.timedelta(days=i) for i in range(1, 16)]  # Extend for 15 days ahead
+    future_dates = [all_dates[-1] + datetime.timedelta(days=i) for i in range(1, 6)]  # Extend for 15 days ahead
     
     # Create a continuous range of days for the entire graph, including future dates
-    all_days = np.arange(0, len(days) + 15).reshape(-1, 1)
+    all_days = np.arange(0, len(days) + 5).reshape(-1, 1)
     all_X_poly = poly.transform(all_days)
     all_y_pred = model.predict(all_X_poly)
+
+    # Calculate the predicted prices for the future dates
+    future_y_pred = all_y_pred[len(days):]
 
     # Plot historical stock prices
     plt.figure(figsize=(10, 6))
     plt.plot(all_dates, closing_prices, label=f'{stock_symbol} Closing Price', color='blue')
 
     # Plot the polynomial regression model line throughout the entire graph
-    plt.plot(np.concatenate((all_dates, future_dates)), np.concatenate((all_y_pred[:len(days)], all_y_pred[len(days):])), label='Predicted Prices', color='red')
+    plt.plot(np.concatenate((all_dates, future_dates)), np.concatenate((all_y_pred[:len(days)], future_y_pred)), label='Predicted Prices', color='red')
 
     plt.xlabel('Date')
     plt.ylabel('Price (USD)')
@@ -60,19 +71,33 @@ def generate_and_save_stock_graph(stock_symbol):
     plt.close()
 
     img_base64 = base64.b64encode(img.read()).decode()
-    return img_base64
+    
+    # Get the current price and round it to 2 decimal places
+    current_price = round(stock_data["Close"].iloc[-1], 2)
+
+    # Get the predicted price and round it to 2 decimal places
+    predicted_price = round(all_y_pred[-1], 2)
+
+    return img_base64, current_price, predicted_price
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     stock_symbol = "AAPL"  # Default stock symbol
     img_data = None
+    current_price = None
+    predicted_price = None
 
     if request.method == 'POST':
         stock_symbol = request.form['stock_symbol']
-    
-    img_data = generate_and_save_stock_graph(stock_symbol)
+        selected_model = request.form['model']  # Retrieve the selected model
+        
+        # Get the selected machine learning model from the dictionary
+        selected_ml_model = models[selected_model]
 
-    return render_template('index.html', img_data=img_data)
+        # Generate the graph and update predicted prices using the selected model
+        img_data, current_price, predicted_price = generate_and_save_stock_graph(stock_symbol, selected_ml_model)
+
+    return render_template('index.html', img_data=img_data, current_price=current_price, predicted_price=predicted_price)
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
